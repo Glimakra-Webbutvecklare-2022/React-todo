0c2509fd05881208a4b77333a9787326f3bbe347
Henry Bergström <henrybergstrom@protonmail.com>
2023-09-04 11:03:21 +0200

    efter genomgång

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  // stateful array to keep track on todos
+  const [todos, setTodos] = useState([{text: 'Plugga react 1 timme', isEditable: false}]);
+  // let todos = ['plugga react 1 timme'];
+
+  // function to addTodo
+  const addTodo = (evt) => {
+    evt.preventDefault(); // stop form from redirecting
+    // todos.push(todoToAdd);
+
+    const todoToAdd = { text: evt.target.elements.todo.value, isEditable: false}; // extract text input
+    evt.target.elements.todo.value = ""; // reset text input
+
+    const newTodos = [...todos, todoToAdd]; // create new array as todos
+
+    setTodos(newTodos); // set new todos and rerender UI
+  }
+
+  // function to removeTodo
+  const removeTodo = (todoToRemove) => {
+    const filteredTodos = todos.filter(item => item.text !== todoToRemove.text); // keep all todos except todoToRemove
+
+    setTodos(filteredTodos);
+  }
+
+  const editTodo = (todoToEdit) => {
+    console.log("I should edit ", todoToEdit);
+
+    const filteredTodos = todos.filter(item => item.text !== todoToEdit.text); // keep all todos except todoToRemove
+
+    const newTodos = [...filteredTodos, {text: todoToEdit.text, isEditable: !todoToEdit.isEditable}]
+
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>My todo app</h1>
+      {/* TodoInput */}
+      <form onSubmit={addTodo}>
+        <input type="text" name="todo" />
+        <button type="submit">Add</button>
+      </form>
+
+      {/* TodoList */}
+      <ul>
+        {/* map todos into li */}
+        {todos.map(item => (<li key={item}> <span contentEditable={item.isEditable}>{item.text}</span> <button onClick={() => removeTodo(item)}>X</button> 
+                                                 <button onClick={() => editTodo(item)}>E</button>  </li>))}
+      </ul>
+    </main>
   );
 }
 
